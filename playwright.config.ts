7f8c446bfdb576dc566cc38efc0538121c3cd95d
Keito654 <38161062+Keito654@users.noreply.github.com>
2024-02-10 02:46:28 +0900

    change config

--- playwright.config.ts
+++ playwright.config.ts
@@ -69,9 +69,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: "pnpm run dev",
-  //   url: "http://localhost:5173",
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: "pnpm run dev",
+    url: "http://localhost:5173",
+    reuseExistingServer: !process.env.CI,
+  },
 });

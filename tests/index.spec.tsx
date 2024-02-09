8c7b8b2f6412d834cc75932eeca20eea1a836e18
Keito654 <38161062+Keito654@users.noreply.github.com>
2024-02-10 02:45:06 +0900

    add test

---
+++ tests/index.spec.tsx
@@ -0,0 +1,28 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("test", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/");
+  });
+
+  test("preserve text state", async ({ page }) => {
+    const input = page.getByPlaceholder("textarea");
+    await input.fill("add test state");
+    await page.getByRole("link", { name: "to second page" }).click();
+
+    await page.getByRole("link", { name: "TOP" }).click();
+    const inputted = await page.getByPlaceholder("textarea").inputValue();
+
+    expect(inputted).toBe("add test state");
+  });
+
+  test("preserve obj state", async ({ page }) => {
+    const button = page.getByRole("button", { name: "obj button" });
+    await button.click();
+    await page.getByRole("link", { name: "to second page" }).click();
+
+    await page.getByRole("link", { name: "TOP" }).click();
+
+    await expect(page.getByText("name:test, check:true")).toBeVisible();
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 5173,
+  },
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
